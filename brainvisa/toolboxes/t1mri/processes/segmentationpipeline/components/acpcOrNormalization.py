60ca6e908e9aa5d6251521b46549a67720a1b05d
dgeffroy <dgeffroy@users.noreply.github.com>
2010-03-10 12:41:20 +0000

    #1125 In AC/PC or Normalization pipeline, remove inner link between t1mri and commissure_coordinates in Talairach from normalization step.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/t1mri/t1mri-gpl/trunk@36881 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/t1mri/processes/segmentationpipeline/components/acpcOrNormalization.py
+++ brainvisa/toolboxes/t1mri/processes/segmentationpipeline/components/acpcOrNormalization.py
@@ -99,7 +99,8 @@ def initialization( self ):
     if ps:
       eNode1.TalairachFromNormalization.removeLink( 'Commissure_coordinates',
         'Talairach_transform' )
-
+    eNode1.TalairachFromNormalization.removeLink( 't1mri', 'Commissure_coordinates' )
+    
     eNode.addLink( 'Normalization.TalairachFromNormalization.t1mri', 'T1mri' )
     eNode.addLink( 'T1mri', 'Normalization.TalairachFromNormalization.t1mri' )
     eNode.addLink( \
