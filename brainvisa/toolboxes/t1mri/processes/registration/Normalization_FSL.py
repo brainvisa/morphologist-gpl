248e2c23dd75583528616c4905a7b60c94d57e42
denisri <denis.riviere.gm@gmail.com>
2010-11-27 20:05:46 +0000

    moving FSL T1 normalization process to T1 toolbox
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/t1mri/t1mri-gpl/trunk@40079 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/t1mri/processes/registration/Normalization_FSL.py
@@ -0,0 +1,101 @@
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the 
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info". 
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or 
+# data to be ensured and,  more generally, to use and operate it in the 
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
+from neuroProcesses import *
+import shfjGlobals
+import registration
+import os
+try:
+  from neurospy.bvfunc import FSL_pre_processing
+  nipyok=1
+except:
+  nipyok=0
+
+
+def validation():
+  import distutils.spawn
+  if not distutils.spawn.find_executable( 'flirt' ):
+    raise ValidationError(_t_('FSL flirt commandline could not be found'))
+  if not nipyok:
+    raise ValidationError(_t_('FSL_pre_processing could not be loaded'))
+  return 0
+
+name = 'Step 3 : Anatomy Normalization (using FSL)'
+userLevel = 0
+
+signature = Signature(
+  'anatomy_data', ReadDiskItem( "Raw T1 MRI", ['NIFTI-1 image', 'gz compressed NIFTI-1 image']),
+  'anatomical_template', ReadDiskItem( "anatomical Template", ['NIFTI-1 image', 'gz compressed NIFTI-1 image'] ),
+  'Alignment', Choice('Already Virtualy Aligned', 'Not Aligned but Same Orientation', 'Incorrectly Oriented'),
+  'transformation_matrix', WriteDiskItem("FSL Transformation", 'Matlab file'),
+  'normalized_anatomy_data', WriteDiskItem( "Raw T1 MRI", ['gz compressed NIFTI-1 image']),
+)
+
+def initialization( self ):
+  def anat2results(self, process):
+    if self.anatomy_data != None:
+      process.signature["normalized_anatomy_data"].requiredAttributes["normalized"] = "yes"
+      process.signature["normalized_anatomy_data"].requiredAttributes["normalization"] = "FSL"
+    return self.anatomy_data
+  self.linkParameters("transformation_matrix", "anatomy_data")
+  self.linkParameters("normalized_anatomy_data", "anatomy_data", anat2results)
+  self.linkParameters("normalized_anatomy_data", "anatomy_data")
+  self.Alignment = 'Not Aligned but Same Orientation'
+  self.anatomical_template = self.signature[ 'anatomical_template' ].findValue(
+    { 'database' : os.path.join( os.getenv( 'FSLDIR', 'data' ) ),
+      'skull_stripped' : 'no' } )
+
+def execution( self, context ):
+  anat = self.anatomy_data.fullPath()
+  template = self.anatomical_template.fullPath()
+  snmat = self.transformation_matrix.fullPath()
+  normanat = self.normalized_anatomy_data.fullPath()
+  if self.Alignment == 'Already Virtualy Aligned':
+    s1 = 'AVA'
+  elif self.Alignment == 'Not Aligned but Same Orientation':
+    s1 = 'NASO'
+  elif self.Alignment == 'Incorrectly Oriented':
+    s1 = 'IO'
+  FSL_pre_processing.NormalizeAnat(anat, template, normanat, snmat, s1)
+  # get image orientations in current formats
+  srcatts = shfjGlobals.aimsVolumeAttributes( self.anatomy_data )
+  srcs2m = srcatts.get( 'storage_to_memory', None )
+  if srcs2m:
+    self.transformation_matrix.setMinf( 'source_storage_to_memory', srcs2m )
+  dstatts = shfjGlobals.aimsVolumeAttributes( self.anatomical_template )
+  dsts2m = dstatts.get( 'storage_to_memory', None )
+  if dsts2m:
+    self.transformation_matrix.setMinf( 'destination_storage_to_memory',
+    dsts2m )
+  self.transformation_matrix.saveMinf()
+  tm = registration.getTransformationManager()
+  tm.copyReferential( self.anatomical_template, self.normalized_anatomy_data )
