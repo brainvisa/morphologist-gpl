856b9a9e81ca9337f70c657665de30dfc465a24c
denisri <denis.riviere.gm@gmail.com>
2012-02-24 13:44:02 +0000

    transformed parallelRecognition: it now builds a pipeline with parallel execution nodes (sort of map/reduce schema) which can be run through soma-workflow. Iterations have also been modified to allow building a super-pipeline with iterations on the parallelized recognitions.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@47716 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/Sulci/Recognition/parallelRecognition.py
+++ brainvisa/toolboxes/morphologist/processes/Sulci/Recognition/parallelRecognition.py
@@ -44,7 +44,8 @@ signature = Signature(
     WriteDiskItem( 'Labelled Cortical folds graph', 'Graph', 
                    requiredAttributes = { 'labelled' : 'Yes',
                                           'automatically_labelled' \
-                                          : 'Yes' } ),
+                                          : 'Yes'} ),
+    'model_hint', Choice( ( 'Newer (2008)', 0 ), ( 'Older (2001)', 1 ) ),
     'number_of_trials', Integer(), 
     'keep_all_results', Boolean(),
     'energy_plot_file', WriteDiskItem( 'siRelax Fold Energy', 'siRelax Fold Energy' ),
@@ -54,11 +55,66 @@ signature = Signature(
     'niterBelowStopProp', Integer(),
 )
 
+
+def modelValue(self, proc, procbis ):
+  """
+  the input graph which is linked to the model has not necessarily the same
+  graph_version but this attribute is a key for Graph data, so it will be seen
+  as a required attribute, so it is removed from the link information
+  """
+  val=None
+  if proc.data_graph:
+    val=proc.data_graph.attributes()
+    if val.has_key( 'graph_version' ):
+      del val['graph_version']
+  rdi = ReadDiskItem( 'Model graph', 'Graph',
+        requiredAttributes={'trained' : 'Yes'} )
+  mod = list( rdi._findValues( val, None, False ) )
+  db = [ x.get( 'sulci_database' ) for x in mod ]
+  dind = [ i for i in range(len(db)) if db[i] is not None ]
+  db = [ int(x) for x in db if x is not None ]
+  newind = [ dind[db.index(x)] for x in sorted( db ) ]
+  if len( mod ) == 1:
+    return val
+  if len( mod ) > self.model_hint:
+    return mod[newind[len( mod ) - self.model_hint - 1]]
+  return None
+
+
+def _iterateAccept( self ):
+    # Here we use a modified iteration system: instead of generating an
+    # iteration on parallelRecognition, we use the results of the
+    # parallelRecognition iteration (which only generates processes)
+    # and build a new iteration from these results.
+    try:
+        params = self._iterationDialog.getLists()
+        processes = self.process._iterate( **params )
+        iterationProcess = neuroProcesses.IterationProcess( self.process.name+" iteration", processes )
+        procs = defaultContext().runProcess( iterationProcess )
+        iteration2 = neuroProcesses.IterationProcess( self.process.name+" iteration", procs )
+        showProcess( iteration2 )
+    except:
+        neuroException.showException()
+        self._iterationDialog.show()
+
+
+def inlineGUI( self, proc, procview, externalRunButton = True ):
+    # we redefine inlineGUI on this process to modify the iteration behaviour
+    # (see _iterAccept above)
+    gui = procview.defaultInlineGUI( None )
+    # rebind the _iterateAccept method of this Process class (defined here in
+    # this source) to the process view instance
+    procview._iterateAccept = self.__class__._iterateAccept.__get__( procview )
+    print procview._iterateAccept
+    return gui
+
+
 def initialization( self ):
-    self.setOptional( 'stats_file' )
-    self.linkParameters( 'model', 'data_graph' )
+    self.linkParameters( 'model', ( 'data_graph', 'model_hint' ),
+        self.modelValue )
     self.linkParameters( 'output_graph', 'data_graph' )
     self.linkParameters( 'energy_plot_file', 'output_graph' )
+    self.setOptional( 'stats_file' )
     self.linkParameters( 'stats_file', 'output_graph' )
     self.number_of_trials = 10
     self.keep_all_results = 0
@@ -68,6 +124,7 @@ def initialization( self ):
     self.rate = 0.98
     self.stopRate = 0.05
     self.niterBelowStopProp = 1
+
 
 def execution( self, context ):
     if self.keep_all_results:
@@ -79,11 +136,14 @@ def execution( self, context ):
     stats = []
     energies = []
     nrjfiles = []
-    if self.keep_all_results:
-      pn = ParallelExecutionNode( 'ParallelRecognition' )
-    else:
-      pn = SerialExecutionNode( 'ParallelRecognition' )
-      context.write( 'executing in serial mode when keep_all_results is False' )
+    pn = ParallelExecutionNode( 'ParallelRecognition' )
+
+    pproc = Process()
+    pproc._id = 'MultipleANNRecognition'
+    pproc.name = 'Multiple ANN Recognition'
+    sign = []
+    pproc.signature = Signature()
+
     for x in xrange( self.number_of_trials ):
         g = os.path.join( dir, '%s_res_%03d.arg' \
                           % ( os.path.basename( self.data_graph.fullName() ),
@@ -95,10 +155,12 @@ def execution( self, context ):
         stats.append( s )
         context.write( str(x), ': ', g, '\n' )
         name = 'recognition_%d' % x
-        pn.addChild( name, ProcessExecutionNode( 'recognition' ) )
+        pn.addChild( name, ProcessExecutionNode( 'recognition', optional=True,
+            selected=True ) )
         proc = getattr( pn, name )
         proc.data_graph = self.data_graph
         proc.model = self.model
+        proc.model_hint = self.model_hint
         proc.output_graph = g
         proc.energy_plot_file = s
         proc.rate = self.rate
@@ -106,16 +168,29 @@ def execution( self, context ):
         proc.niterBelowStopProp = self.niterBelowStopProp
 
         nrjfiles.append( s )
-    context.write( 'running ParallelRecognition nodes' )
-    pn.run( context )
-    context.write( 'ParallelRecognition nodes done' )
+
+    eNode = SerialExecutionNode( 'ParallelRecognitionPipeline', optional=True,
+        selected=True )
+    eNode.addChild( 'recognition', pn )
 
     # find and keep the best one
     graphs = [ ReadDiskItem( 'Labelled Cortical folds graph',
         'Graph And Data' ).findValue( g ) for g in res ]
     energies = [ ReadDiskItem( 'siRelax Fold Energy',
         'siRelax Fold Energy' ).findValue( s ) for s in stats ]
-    context.runProcess( 'chooseBestRecognition', labelled_graphs=graphs,
-        energy_plot_files=energies, output_graph=self.output_graph,
-        energy_plot_file=self.energy_plot_file, stats_file=self.stats_file )
 
+    choose = ProcessExecutionNode( 'chooseBestRecognition', optional=True,
+        selected=True )
+    eNode.addChild( 'chooseBestRecognition', choose )
+    proc = getattr( eNode, 'chooseBestRecognition' )
+    proc.labelled_graphs = graphs
+    proc.energy_plot_files = energies
+    proc.output_graph = self.output_graph
+    proc.energy_plot_file = self.energy_plot_file
+    proc.stats_file = self.stats_file
+
+    pproc.setExecutionNode( eNode )
+
+    if hasattr( context, 'inlineGUI' ):
+        pv = mainThreadActions().call( ProcessView, pproc )
+    return pproc
