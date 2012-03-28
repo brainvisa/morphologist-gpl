4773d2d8767175a3cea15224d44fa2727966f875
denisri <denis.riviere.gm@gmail.com>
2012-03-28 13:38:09 +0000

    - New process: AnatomistShowSymmetrizedData, to view in Anatomist symmetrized objects. Objects are symmetrized according to the AC/PC referential plane, so must have a transformation (direct or indirect) to the AC/PC standard ref. - New process: AnatomistShowSymmetrizedFoldGraph, a specialization of the above process for folds graphs: show a symmetrized folds graph with hemisphere G/W mesh.
    Fixes: #4603
    
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@48303 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/morphologist/processes/viewers/AnatomistShowSymmetrizedData.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
+from neuroProcesses import *
+from brainvisa import anatomist
+import registration
+from soma import aims
+import numpy
+from neuroHierarchy import databases
+
+name = 'Anatomist Show Symmetrized data'
+#roles = ('viewer',)
+userLevel = 2
+
+def validation():
+    anatomist.validation()
+
+signature = Signature(
+ 'data_type', Choice( 'Any Type' ),
+ 'items', ListOf( ReadDiskItem( 'Any Type', getAllFormats() ) ),
+)
+
+
+def dataTypeChanged(self, dataType):
+  if dataType:
+    formats=list(databases.getTypesFormats(dataType))
+    if not formats:
+      formats=getAllFormats()
+    self.signature['items'] = ListOf( ReadDiskItem( dataType, formats ) )
+    self.signatureChangeNotifier.notify(self)
+
+
+def initialization( self ):
+  possibleTypes = [ t.name for t in getAllDiskItemTypes() ]
+  self.signature[ 'data_type' ].setChoices(*sorted(possibleTypes))
+  self.data_type='Any Type'
+  self.addLink( 'items', 'data_type' , self.dataTypeChanged )
+
+
+def execution( self, context ):
+  a = anatomist.Anatomist()
+  alive = []
+  objs = []
+  reftable = {}
+  tm = registration.getTransformationManager()
+  acpc = tm.referential( registration.talairachACPCReferentialId )
+  mni = tm.referential( registration.talairachMNIReferentialId )
+  for data in self.items:
+    ad = a.loadObject( data, forceReload=True )
+    alive.append( ad )
+    ref = tm.referential( data )
+    if ref is None:
+      context.write( 'Warning: no referential on data %s, assuming ACPC ref.' \
+        % data.fullPath() )
+      ref = acpc
+    oref = reftable.get( ref )
+    if oref:
+      ad.assignReferential( oref )
+      continue
+    pr = aims.AffineTransformation3d()
+    pth = tm.findPaths( ref.uuid(), acpc.uuid() )
+    dest = a.centralRef
+    for pt in pth:
+      break
+    else:
+      pth = tm.findPaths( ref.uuid(), mni.uuid() )
+      for pt in pth:
+        dest = a.mniTemplateRef
+        break
+      else:
+        context.write( 'no path.' )
+        pt = []
+    for p in pt:
+      pr2 = aims.read( p.fullPath() )
+      pr = pr2 * pr
+    if dest == a.mniTemplateRef:
+      pth = tm.findPaths( acpc.uuid(), mni.uuid() )
+      for pt in pth:
+        break
+      for p in pt:
+        pr2 = aims.read( p.fullPath() )
+        pr = pr2.inverse() * pr
+      dest = a.centralRef
+    inv = aims.Motion( [ -1, 0, 0, 0,  0, 1, 0, 0,  0, 0, 1, 0,  0, 0, 0, 1 ] )
+    pr = inv * pr
+    trans = list( pr.translation() ) + list( numpy.array( \
+      pr.rotation().volume(), copy=False ).ravel() )
+    trans = [ float( x ) for x in trans ]
+    oref = a.createReferential()
+    newid = a.newId()
+    a.execute( 'LoadTransformation', origin=oref, destination=dest,
+      matrix=trans, res_pointer=newid )
+    ad.setMaterial( front_face="counterclockwise" )
+    reftable[ ref ] = oref
+    ad.assignReferential( oref )
+  if len( alive ) != 0:
+    w = a.createWindow( '3D' )
+    w.addObjects( alive )
+    alive.append( w )
+  return alive
+
---
+++ brainvisa/toolboxes/morphologist/processes/viewers/Sulci/AnatomistShowSymmetrizedFoldGraph.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
+from neuroProcesses import *
+from brainvisa import anatomist
+import registration
+from soma import aims
+import numpy
+from neuroHierarchy import databases
+
+name = 'Anatomist Show Symmetrized Folds Graph'
+#roles = ('viewer',)
+userLevel = 2
+
+def validation():
+    anatomist.validation()
+
+signature = Signature(
+    'graph', ReadDiskItem( 'Cortical folds graph', 'Graph' ),
+    'nomenclature', ReadDiskItem( 'Nomenclature', 'Hierarchy' ),
+    'white_mesh', ReadDiskItem( 'Hemisphere White Mesh',
+      'Anatomist mesh formats' ),
+    )
+
+def initialization( self ):
+    self.setOptional( 'nomenclature' )
+    self.setOptional( 'white_mesh' )
+    self.linkParameters( 'white_mesh', 'graph' )
+    self.nomenclature = self.signature[ 'nomenclature' ].findValue( {} )
+
+def execution( self, context ):
+    toload = [ self.graph ]
+    if self.white_mesh is not None:
+      toload.append( self.white_mesh )
+    selfdestroy = []
+    if self.nomenclature is not None:
+        ( hie, br ) = context.runProcess( 'AnatomistShowNomenclature',
+                                          read=self.nomenclature )
+        selfdestroy += ( hie, br )
+    objs = context.runProcess( 'AnatomistShowSymmetrizedData', items=toload )
+    a = anatomist.Anatomist()
+    l = self.graph.get( 'automatically_labelled' )
+    context.write( 'automatically_labelled:', l )
+    nomenclatureprop = 'default'
+    if l and l == 'Yes':
+        nomenclatureprop = 'label'
+        #a.setGraphParams( label_attribute='label', use_nomenclature=1 )
+    else:
+        l = self.graph.get( 'manually_labelled' )
+        context.write( 'manually_labelled:', l )
+        if l and l == 'Yes':
+            nomenclatureprop = 'name'
+            #a.setGraphParams( label_attribute='name', use_nomenclature=1 )
+    graph = objs[0]
+    win = objs[-1]
+    win.addObjects( graph, add_graph_nodes=True )
+    context.write( 'nomenclature_property:', nomenclatureprop )
+    a.execute( 'GraphDisplayProperties', objects=[graph],
+      nomenclature_property=nomenclatureprop )
+    selfdestroy += objs
+    return selfdestroy
+
