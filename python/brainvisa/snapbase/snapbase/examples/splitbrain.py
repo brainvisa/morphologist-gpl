e8fb64a7e75a5ce293c0d18a046295be759aa414
clarafischer <clarafischer@users.noreply.github.com>
2013-02-04 13:51:33 +0000

    change the repartition of the slices.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@53079 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/snapbase/snapbase/examples/splitbrain.py
+++ python/brainvisa/snapbase/snapbase/examples/splitbrain.py
@@ -45,9 +45,13 @@ class SplitBrainSnapBase(SnapBase):
 
         for d in directions :
             d_minmax = (splitbrain_minmax[d][0], splitbrain_minmax[d][1])
-
-            slices_list = range(d_minmax[0], d_minmax[1],
-                (d_minmax[1]-d_minmax[0])/14)[1:13]
+            step = (d_minmax[1]-d_minmax[0])/12
+            remainder = (d_minmax[1]-d_minmax[0]) - step*12
+            first_slice = d_minmax[0] + (step+remainder)/2
+            last_slice = d_minmax[1] - (step+remainder)/2 + 1
+            
+            slices_list = range(first_slice, last_slice, step)
+            
             # This converts each slice index into a list applicable to
                 # Anatomist camera function
             slices[d] = [(i, self.__get_slice_position__(d, i, voxel_size)) for i in slices_list]
@@ -131,9 +135,13 @@ class BrainMaskSnapBase(SnapBase):
 
         for d in directions :
             d_minmax = (brainmask_minmax[d][0], brainmask_minmax[d][1])
-
-            slices_list = range(d_minmax[0], d_minmax[1],
-                (d_minmax[1]-d_minmax[0])/14)[1:13]
+            step = (d_minmax[1]-d_minmax[0])/12
+            remainder = (d_minmax[1]-d_minmax[0]) - step*12
+            first_slice = d_minmax[0] + (step+remainder)/2
+            last_slice = d_minmax[1] - (step+remainder)/2 + 1
+            
+            slices_list = range(first_slice, last_slice, step)
+            
             # This converts each slice index into a list applicable to
                 # Anatomist camera function
             slices[d] = [(i, self.__get_slice_position__(d, i, voxel_size)) for i in slices_list]
