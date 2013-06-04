2d52445aabcd942710fd05c639389a84ceb8c200
clarafischer <clarafischer@users.noreply.github.com>
2013-06-04 10:00:54 +0000

    changed a parameter name and random seed can be fixed through an hidden parameter.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@56213 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/segmentationpipeline/components/GreyWhiteClassificationHemi.py
+++ brainvisa/toolboxes/morphologist/processes/segmentationpipeline/components/GreyWhiteClassificationHemi.py
@@ -35,13 +35,13 @@ from brainvisa.processes import *
 from soma import aims
 import registration
 
-name = 'Grey White Classification 2012, one hemisphere'
+name = 'Hemisphere Grey White Classification'
 userLevel = 0
 
 # Argument declaration
 signature = Signature(
     'side', Choice( "left", "right" ),
-    'mri_corrected', ReadDiskItem( 'T1 MRI Bias Corrected',
+    't1mri_nobias', ReadDiskItem( 'T1 MRI Bias Corrected',
         'Aims readable volume formats' ),
     'histo_analysis', ReadDiskItem( 'Histo Analysis', 'Histo Analysis' ),
     'split_mask', ReadDiskItem( 'Split Brain Mask',
@@ -52,34 +52,45 @@ signature = Signature(
         'Commissure coordinates'),
     'grey_white', WriteDiskItem( 'Morphologist Grey White Mask',
         'Aims writable volume formats' ),
+    'fix_random_seed', Boolean(),
 )
+
 # Default values
 def initialization( self ):
     def linkGW( self, dummy ):
-        if self.mri_corrected is not None:
-            return self.signature[ 'grey_white' ].findValue( self.mri_corrected, requiredAttributes={ 'side': self.side } )
-    self.linkParameters( 'histo_analysis', 'mri_corrected' )
-    self.linkParameters( 'grey_white', ( 'mri_corrected', 'side' ), linkGW )
-    self.linkParameters( 'split_mask', 'mri_corrected' )
-    self.linkParameters( 'edges', 'mri_corrected' )
-    self.linkParameters( 'commissure_coordinates', 'mri_corrected' )
+        if self.t1mri_nobias is not None:
+            return self.signature[ 'grey_white' ].findValue( self.t1mri_nobias, requiredAttributes={ 'side': self.side } )
+    self.linkParameters( 'histo_analysis', 't1mri_nobias' )
+    self.linkParameters( 'grey_white', ( 't1mri_nobias', 'side' ), linkGW )
+    self.linkParameters( 'split_mask', 't1mri_nobias' )
+    self.linkParameters( 'edges', 't1mri_nobias' )
+    self.linkParameters( 'commissure_coordinates', 't1mri_nobias' )
+    self.signature[ 'fix_random_seed' ].userLevel = 3
+    self.fix_random_seed = False
 
 
 def execution( self, context ):
-    tm=registration.getTransformationManager()
-
-    context.write( "Computing hemisphere grey-white classification..." )
-    if self.side == 'left':
-        sideval = 2
+    if os.path.exists(self.grey_white.fullName() + '.loc'):
+        context.write(self.grey_white.fullName(), 'has been locked')
+        context.write('Remove', self.grey_white.fullName(), '.loc if you want to trigger a new classification')
     else:
-        sideval = 1
-    context.system( "VipGreyWhiteClassif", "-i",
-                    self.mri_corrected, "-h",
-                    self.histo_analysis, "-m",
-                    self.split_mask, "-edges",
-                    self.edges, "-P",
-                    self.commissure_coordinates, "-o",
-                    self.grey_white, "-l", sideval,
-                    "-w", "t", "-a", "N" )
-    tm.copyReferential(self.mri_corrected, self.grey_white)
+        context.write( "Computing hemisphere grey-white classification..." )
+        if self.side == 'left':
+            sideval = 2
+        else:
+            sideval = 1
+        command = [ 'VipGreyWhiteClassif', '-i',
+                    self.t1mri_nobias, '-h',
+                    self.histo_analysis, '-m',
+                    self.split_mask, '-edges',
+                    self.edges, '-P',
+                    self.commissure_coordinates, '-o',
+                    self.grey_white, '-l', sideval,
+                    '-w', 't', '-a', 'N' ]
+        if self.fix_random_seed:
+            command += ['-srand', '10']
+        context.system( *command )
+        
+        tm = registration.getTransformationManager()
+        tm.copyReferential(self.t1mri_nobias, self.grey_white)
 
