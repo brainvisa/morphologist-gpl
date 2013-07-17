84f687d0765c99d4d84544dbe7903ab739478edd
operto <operto@bioproj.invalid>
2013-07-17 13:21:21 +0000

    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@57208 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/checkbase/check.py
+++ python/brainvisa/checkbase/check.py
@@ -89,7 +89,10 @@ def json2html(json, embedded_data=None, with_head_tags=True):
         bgcolor = '#FFFFFF'
         for each in json['key_items']:
            bgcolor = color[each in items]
-           html += "   <td bgcolor=%s data-aux='%s' class='b-table__cell'>&nbsp;</td>"%(bgcolor, embedded_data[subject][each])
+           emb_data = ''
+           if embedded_data.has_key(subject) and embedded_data[subject].has_key(each):
+              emb_data = embedded_data[subject][each]
+           html += "   <td bgcolor=%s data-aux='%s' class='b-table__cell'>&nbsp;</td>"%(bgcolor, emb_data)
         html += "      </tr>"
    html += "</table>"
    if with_head_tags:
