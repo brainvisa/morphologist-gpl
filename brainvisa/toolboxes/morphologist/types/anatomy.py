a1b86565b0e95c865c295ef31eea2d679ca7e484
mbouin <mbouin@bioproj.invalid>
2014-10-22 09:40:23 +0000

    include snapshots types
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@63937 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/types/anatomy.py
+++ brainvisa/toolboxes/morphologist/types/anatomy.py
@@ -35,6 +35,7 @@
 
 include( 'builtin' )
 include( 'registration' )
+include( 'snapshots' )
 
 Format( 'Commissure coordinates', "f|*.APC" )
 Format( 'Histogram', 'f|.his' )
@@ -198,26 +199,3 @@ FileType( 'anatomical Mask Template', '3D Volume')
 #----------- Obsololete registration ----------------------
 
 FileType( 'MINC transformation matrix', 'Any Type', 'MINC transformation matrix' )
-
-#----------- Snapshots with snapbase ----------------------
-
-FileType('Snapshot Grey White', '2D Image', 'PNG image')
-FileType('Snapshot Split Brain', '2D Image', 'PNG image')
-FileType('Snapshot Meshcut', '2D Image', 'PNG image')
-FileType('Snapshot Pial Mesh', '2D Image', 'PNG image')
-FileType('Snapshot Sulci', '2D Image', 'PNG image')
-FileType('Snapshot White Mesh', '2D Image', 'PNG image')
-FileType('Snapshot Brain Mask', '2D Image', 'PNG image')
-FileType('Snapshot Raw T1', '2D Image', 'PNG image')
-FileType('Snapshot Tablet Raw T1', '2D Image', 'PNG image')
-FileType('Snapshots Grey White Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshots Split Brain Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshots Meshcut Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshots Pial Mesh Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshots Sulci Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshots White Mesh Quality Scores', 'CSV file', 'CSV file')
-FileType('Snapshot Grey Matter', '2D Image', 'PNG image')
-FileType('Snapshot White Matter', '2D Image', 'PNG image')
-FileType('Snapshot CSF', '2D Image', 'PNG image')
-FileType('Snapshots SPM Quality Scores', 'CSV file', 'CSV file')
-
