a47408798c81c2708ddb132abe76930b2e794c39
clarafischer <clarafischer@users.noreply.github.com>
2015-01-26 11:55:39 +0000

    change the type of the skull_stripped image.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@65394 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/tools/skullstripping.py
+++ brainvisa/toolboxes/morphologist/processes/tools/skullstripping.py
@@ -37,18 +37,18 @@ name = 'Skull-stripping'
 userLevel = 2
 
 signature = Signature(
-    't1mri', ReadDiskItem( "Raw T1 MRI", "Aims readable volume formats" ),
-    'brain_mask', ReadDiskItem( "Brain Mask", "Aims readable volume formats" ),
-    'skull_stripped', WriteDiskItem("3D Volume",
-      'Aims writable volume formats' ),
+    't1mri', ReadDiskItem('Raw T1 MRI', 'Aims readable volume formats'),
+    'brain_mask', ReadDiskItem('Brain Mask', 'Aims readable volume formats'),
+    'skull_stripped', WriteDiskItem('Raw T1 MRI Brain Masked',
+        'Aims writable volume formats'),
 )
 
 def initialization( self ):
-    self.linkParameters("brain_mask", "t1mri" )
-    #self.linkParameters("skull_stripped", "t1mri" )
+    self.linkParameters("brain_mask", "t1mri")
+    self.linkParameters("skull_stripped", "t1mri")
 
 def execution( self, context ):
-    context.system( 'AimsMask', '-i', self.t1mri, '-m', self.brain_mask,
-        '-o', self.skull_stripped )
+    context.system('AimsMask', '-i', self.t1mri, '-m', self.brain_mask,
+        '-o', self.skull_stripped)
 
 
