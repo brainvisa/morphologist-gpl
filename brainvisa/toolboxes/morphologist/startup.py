5a2939b5f169f07317b4ea12e8c7da59a864dd4e
denisri <denis.riviere.gm@gmail.com>
2015-01-28 13:07:12 +0000

    modifs (+ PEP8izaton) for SPM12 database organization
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@65435 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/startup.py
+++ brainvisa/toolboxes/morphologist/startup.py
@@ -46,44 +46,45 @@ configuration = Application().configuration
 # if FSL is present: setup a database for FSL templates
 fsldir = configuration.FSL.fsldir
 if not fsldir:
-  fsldir = os.getenv( 'FSLDIR' )
+    fsldir = os.getenv('FSLDIR')
 if not fsldir and distutils.spawn.find_executable( 'fslview' ):
-  # probably a system-wide linux installation like on Ubuntu
-  if os.path.isdir( '/usr/share/fsl/data' ):
-    fsldir = '/usr/share/fsl'
-    if not configuration.FSL.fsl_commands_prefix \
-      and not distutils.spawn.find_executable( 'flirt' ):
-        versions = [ v for v in os.listdir( os.path.join( fsldir ) ) \
-          if v != 'data' ]
-        versionsi = [ v.split( '.' ) for v in versions ]
-        try:
-          versionsi = [ int( v[0] ) * 0x100 + int( v[1] ) for v in versionsi ]
-          version = versionsi.index( max( versionsi ) )
-          fsl_prefix = 'fsl' + versions[ version ] + '-'
-          if distutils.spawn.find_executable( fsl_prefix + 'flirt' ):
-            configuration.FSL.fsl_commands_prefix = fsl_prefix
-        except:
-          print 'could not read FSL versions'
-if fsldir and os.path.exists( fsldir ):
-  fslshare = os.path.join( fsldir, 'data' )
-  if os.path.exists( fslshare ):
-    if fslshare not in [ x.directory for x in neuroConfig.dataPath ]:
-      dbs = neuroConfig.DatabaseSettings( fslshare )
-      #fsldb = os.path.join( neuroConfig.homeBrainVISADir, 'fsl' )
-      #if not os.path.exists( fsldb ):
-        #os.mkdir( fsldb )
-      dbs.expert_settings.ontology = 'fsl'
-      dbs.expert_settings.sqliteFileName = ':temporary:'
-      dbs.expert_settings.uuid = 'a69ed62b-4895-4245-b42a-d211e1c6faa4'
-      dbs.builtin = True
-      neuroConfig.dataPath.insert( 1, dbs )
-      db = neuroHierarchy.SQLDatabase( dbs.expert_settings.sqliteFileName, fslshare, 
-                                       'fsl', context=brainvisa.processes.defaultContext(), settings=dbs )
-      neuroHierarchy.databases.add( db )
-      neuroHierarchy.update_soma_workflow_translations()
+    # probably a system-wide linux installation like on Ubuntu
+    if os.path.isdir('/usr/share/fsl/data'):
+        fsldir = '/usr/share/fsl'
+        if not configuration.FSL.fsl_commands_prefix \
+                and not distutils.spawn.find_executable('flirt'):
+            versions = [v for v in os.listdir(os.path.join(fsldir)) \
+                if v != 'data']
+            versionsi = [v.split('.') for v in versions]
+            try:
+                versionsi = [int(v[0]) * 0x100 + int(v[1]) for v in versionsi]
+                version = versionsi.index(max(versionsi))
+                fsl_prefix = 'fsl' + versions[version] + '-'
+                if distutils.spawn.find_executable(fsl_prefix + 'flirt'):
+                    configuration.FSL.fsl_commands_prefix = fsl_prefix
+            except:
+                print 'could not read FSL versions'
+if fsldir and os.path.exists(fsldir):
+    fslshare = os.path.join(fsldir, 'data')
+    if os.path.exists(fslshare):
+        if fslshare not in [x.directory for x in neuroConfig.dataPath]:
+            dbs = neuroConfig.DatabaseSettings(fslshare)
+            #fsldb = os.path.join( neuroConfig.homeBrainVISADir, 'fsl' )
+            #if not os.path.exists( fsldb ):
+                #os.mkdir( fsldb )
+            dbs.expert_settings.ontology = 'fsl'
+            dbs.expert_settings.sqliteFileName = ':temporary:'
+            dbs.expert_settings.uuid = 'a69ed62b-4895-4245-b42a-d211e1c6faa4'
+            dbs.builtin = True
+            neuroConfig.dataPath.insert(1, dbs)
+            db = neuroHierarchy.SQLDatabase(
+                dbs.expert_settings.sqliteFileName, fslshare, 'fsl',
+                context=brainvisa.processes.defaultContext(), settings=dbs)
+            neuroHierarchy.databases.add(db)
+            neuroHierarchy.update_soma_workflow_translations()
 
-      del dbs, db #, fsldb
-  del fslshare
+            del dbs, db #, fsldb
+    del fslshare
 del fsldir
 
 # if Matlab and SPM are found: setup a database for SPM templates
@@ -91,41 +92,47 @@ del fsldir
 spmscript = None
 spmdir = None
 if configuration.SPM.spm8_standalone_path:
-  spmdir = configuration.SPM.spm8_standalone_path
+    spmdir = configuration.SPM.spm8_standalone_path
 elif configuration.SPM.spm8_path:
-  spmdir = configuration.SPM.spm8_path
+    spmdir = configuration.SPM.spm8_path
 elif configuration.SPM.spm5_path:
-  spmdir = configuration.SPM.spm5_path
+    spmdir = configuration.SPM.spm5_path
+# print '*** SPMDIR:', spmdir
 if spmdir is not None:
-  spmtemplates = None
-  # print 'SPM dir:', spmdir
-  if not os.path.isdir( os.path.join( spmdir, 'templates' ) ):
-    if os.path.isdir( os.path.join( spmdir, 'spm8_mcr', 'spm8',
-      'templates' ) ):
-      spmtemplates = os.path.join( spmdir, 'spm8_mcr', 'spm8' )
-  else:
-    spmtemplates = spmdir #os.path.join( spmdir, 'templates' )
-  #print 'spmtemplates:', spmtemplates
-  if not neuroConfig.fastStart and spmtemplates and \
-    not neuroHierarchy.databases.hasDatabase( spmtemplates ):
-    dbs = neuroConfig.DatabaseSettings( spmtemplates )
-    #spmdb = os.path.join( neuroConfig.homeBrainVISADir, 'spm' )
-    #if not os.path.exists( spmdb ):
-      #os.mkdir( spmdb )
-    dbs.expert_settings.ontology = 'spm'
-    dbs.expert_settings.sqliteFileName = ':temporary:'
-    dbs.expert_settings.uuid = 'a91fd1bf-48cf-4759-896e-afea136c0549'
-    dbs.builtin = True
-    neuroConfig.dataPath.insert( 1, dbs )
-    db = neuroHierarchy.SQLDatabase( dbs.expert_settings.sqliteFileName, spmtemplates, 
-                                     'spm', context=brainvisa.processes.defaultContext(), settings=dbs )
-    neuroHierarchy.databases.add( db )
-    neuroHierarchy.update_soma_workflow_translations()
-    del dbs, db
-  del spmtemplates, spmdir
+    spmtemplates = None
+    #print 'SPM dir:', spmdir
+    if not os.path.isdir(os.path.join(spmdir, 'templates')) \
+            and not os.path.isdir(os.path.join(spmdir, 'toolbox')):
+        if os.path.isdir(os.path.join(spmdir, 'spm8_mcr', 'spm8',
+                                      'templates')):
+            spmtemplates = os.path.join(spmdir, 'spm8_mcr', 'spm8')
+        elif os.path.isdir(os.path.join(spmdir, 'spm12_mcr', 'spm12',
+                                        'toolbox', 'OldNorm')):
+            spmtemplates = os.path.join(spmdir, 'spm12_mcr', 'spm12')
+    else:
+        spmtemplates = spmdir #os.path.join( spmdir, 'templates' )
+    #print 'spmtemplates:', spmtemplates
+    if not neuroConfig.fastStart and spmtemplates and \
+        not neuroHierarchy.databases.hasDatabase(spmtemplates):
+        dbs = neuroConfig.DatabaseSettings(spmtemplates)
+        #spmdb = os.path.join(neuroConfig.homeBrainVISADir, 'spm')
+        #if not os.path.exists(spmdb):
+            #os.mkdir(spmdb)
+        dbs.expert_settings.ontology = 'spm'
+        dbs.expert_settings.sqliteFileName = ':temporary:'
+        dbs.expert_settings.uuid = 'a91fd1bf-48cf-4759-896e-afea136c0549'
+        dbs.builtin = True
+        neuroConfig.dataPath.insert(1, dbs)
+        db = neuroHierarchy.SQLDatabase(
+            dbs.expert_settings.sqliteFileName, spmtemplates, 'spm',
+            context=brainvisa.processes.defaultContext(), settings=dbs)
+        neuroHierarchy.databases.add(db)
+        neuroHierarchy.update_soma_workflow_translations()
+        del dbs, db
+    del spmtemplates, spmdir
 
 #Sulci configuration
 if not configuration.signature.has_key('sulci'):
-  configuration.add( 'sulci', SulciConfiguration() )
+    configuration.add('sulci', SulciConfiguration())
 
 
