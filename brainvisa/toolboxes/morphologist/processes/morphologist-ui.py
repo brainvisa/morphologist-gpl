3ff8e7e8bf3b95d4d1e82af1bc6e722239941ec0
denisri <denis.riviere.gm@gmail.com>
2015-09-03 22:42:58 +0000

    PEP8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@68860 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/morphologist-ui.py
+++ brainvisa/toolboxes/morphologist/processes/morphologist-ui.py
@@ -2,18 +2,18 @@ from brainvisa.processes import *
 from brainvisa.processing.qtgui.backwardCompatibleQt import *
 
 def validation():
-  muiexe = findInPath( 'morphologist' )
-  if not muiexe:
-    raise ValidationError( 'morphologist program is not found' )
+    muiexe = findInPath('morphologist')
+    if not muiexe:
+        raise ValidationError( 'morphologist program is not found' )
 
 name = 'Morphologist UI 2015'
 userLevel = 0
 
-def overrideGUI( self ):
-  muiexe = os.path.join( findInPath( 'morphologist' ), 'morphologist' )
-  defaultContext().system( sys.executable, muiexe )
-  w = QWidget( None )
-  w.setAttribute( Qt.WA_DeleteOnClose )
-  QTimer.singleShot( 0, w.close )
-  return w
+def overrideGUI(self):
+    muiexe = os.path.join(findInPath('morphologist'), 'morphologist')
+    defaultContext().system(sys.executable, muiexe)
+    w = QWidget(None)
+    w.setAttribute(Qt.WA_DeleteOnClose)
+    QTimer.singleShot(0, w.close)
+    return w
 
