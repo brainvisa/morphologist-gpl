b43014946233f996f867ab2aef9abbf6ad5213f2
denisri <denis.riviere.gm@gmail.com>
2015-10-20 15:08:19 +0000

    morphometry inter-subject concatenation process
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@69640 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/morphologist/processes/morphometry/Sulci/sulcigraphmorphometryintersubject.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
+from brainvisa.processes import *
+
+name = 'Sulci graph morphometry inter subject'
+userLevel = 0
+
+signature = Signature(
+    'sulcal_morpho_measures', ListOf(ReadDiskItem(
+        'Sulcal morphometry measurements', 'CSV File')),
+    'subjects', ListOf(String()),
+    'group_morpho_measures',
+        WriteDiskItem('Sulcal morphometry measurements', 'CSV File')
+)
+
+
+def link_subjects(self, proc, dummy):
+    subjects = []
+    for item in self.sulcal_morpho_measures:
+        subjects.append(item.get('subject'))
+    return subjects
+
+
+def initialization( self ):
+    self.linkParameters('subjects', 'sulcal_morpho_measures',
+                        self.link_subjects)
+
+
+def execution( self, context ):
+
+    if len(self.subjects) != len(self.sulcal_morpho_measures):
+        raise ValueError(
+            'subjects list must be the same size as sulcal_morpho_measures')
+
+    f = open(self.group_morpho_measures.fullPath(), 'w')
+    for item, subject in zip(self.sulcal_morpho_measures, self.subjects):
+        ifi = open(item.fullPath())
+        header = ifi.readline()
+        f.write('subject;' + header)
+        for line in ifi.xreadlines():
+            f.write(';'.join([subject, line]))
+
