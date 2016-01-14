52dece6a8667cfefc24d9274fa510ed5feffa556
denisri <denis.riviere.gm@gmail.com>
2016-01-14 14:53:32 +0000

    pipeline processes update (missing optional params)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@71075 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/morphologist/capsul/axon/splitbrain.py
+++ python/morphologist/capsul/axon/splitbrain.py
@@ -12,20 +12,21 @@ from capsul.process import Process
 class SplitBrain(Process):
     def __init__(self, **kwargs):
         super(SplitBrain, self).__init__()
-        self.add_trait('brain_mask', File(allowed_extensions=['.nii.gz', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '']))
-        self.add_trait('t1mri_nobias', File(allowed_extensions=['.nii.gz', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '']))
+        self.add_trait('brain_mask', File(allowed_extensions=['.nii.gz', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.svs', '.vms', '.vmu', '.ndpi', '.scn', '.svslide', '.bif', '']))
+        self.add_trait('t1mri_nobias', File(allowed_extensions=['.nii.gz', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.svs', '.vms', '.vmu', '.ndpi', '.scn', '.svslide', '.bif', '']))
         self.add_trait('histo_analysis', File(allowed_extensions=['.han']))
-        self.add_trait('commissure_coordinates', File(allowed_extensions=['.APC']))
+        self.add_trait('commissure_coordinates', File(allowed_extensions=['.APC'], optional=True))
         self.add_trait('use_ridges', Bool())
-        self.add_trait('white_ridges', File(allowed_extensions=['.nii.gz', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '']))
+        self.add_trait('white_ridges', File(allowed_extensions=['.nii.gz', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.svs', '.vms', '.vmu', '.ndpi', '.scn', '.svslide', '.bif', '']))
         self.add_trait('use_template', Bool())
-        self.add_trait('split_template', File(allowed_extensions=['.nii.gz', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '']))
+        self.add_trait('split_template', File(allowed_extensions=['.nii.gz', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.tif', '.svs', '.vms', '.vmu', '.ndpi', '.scn', '.svslide', '.bif', '']))
         self.add_trait('mode', Enum('Watershed (2011)', 'Voronoi'))
         self.add_trait('variant', Enum('regularized', 'GW Barycentre', 'WM Standard Deviation'))
         self.add_trait('bary_factor', Enum(0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1))
+        self.add_trait('mult_factor', Enum(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, optional=True))
         self.add_trait('initial_erosion', Float())
         self.add_trait('cc_min_size', Int())
-        self.add_trait('split_brain', File(allowed_extensions=['.nii.gz', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', ''], output=True))
+        self.add_trait('split_brain', File(allowed_extensions=['.nii.gz', '.ima', '.dim', '.vimg', '.vinfo', '.vhdr', '.img', '.hdr', '.v', '.i', '.mnc', '.mnc.gz', '.nii', '.jpg', '.gif', '.png', '.mng', '.bmp', '.pbm', '.pgm', '.ppm', '.xbm', '.xpm', '.tiff', ''], output=True))
         self.add_trait('fix_random_seed', Bool())
 
 
@@ -36,6 +37,7 @@ class SplitBrain(Process):
         self.mode = 'Watershed (2011)'
         self.variant = 'GW Barycentre'
         self.bary_factor = 0.6
+        self.mult_factor = 2
         self.initial_erosion = 2.0
         self.cc_min_size = 500
         self.fix_random_seed = False
