e96bf249de7fe2c5f0e6a1b8e331df82965b5db0
denisri <denis.riviere.gm@gmail.com>
2016-04-11 10:04:55 +0000

    Sulci viewer: Avoid ambiguity with global (spam) mesh when a subject has no mesh.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@72561 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/viewers/Sulci/AnatomistShowFoldGraph.py
+++ brainvisa/toolboxes/morphologist/processes/viewers/Sulci/AnatomistShowFoldGraph.py
@@ -43,89 +43,92 @@ def validation():
     anatomist.validation()
 
 signature = Signature(
-    'graph', ReadDiskItem( 'Cortical folds graph', 'Graph' ),
-    'nomenclature', ReadDiskItem( 'Nomenclature', 'Hierarchy' ),
-    'white_mesh', ReadDiskItem( 'Hemisphere White Mesh',
-      'Anatomist mesh formats' ),
-    'hemi_mesh', ReadDiskItem( 'Hemisphere Mesh', 'Anatomist mesh formats' ),
+    'graph', ReadDiskItem('Cortical folds graph', 'Graph'),
+    'nomenclature', ReadDiskItem('Nomenclature', 'Hierarchy'),
+    # the required attribute 'modality' is only here to avoid ambiguity with
+    # the SPAM model mesh when the subject does not have an associated mesh.
+    # It's quite a shameful solution, but...
+    'white_mesh', ReadDiskItem('Hemisphere White Mesh',
+        'Anatomist mesh formats', requiredAttributes={'modality': 't1mri'}),
+    'hemi_mesh', ReadDiskItem('Hemisphere Mesh', 'Anatomist mesh formats'),
     'load_MRI', Choice("Yes","No"),
     'two_windows', Choice("Yes","No"),
-    'mri_corrected', ReadDiskItem( 'T1 MRI Bias Corrected',
-      'Anatomist volume formats' )
+    'mri_corrected', ReadDiskItem('T1 MRI Bias Corrected',
+        'Anatomist volume formats')
     )
 
-def initialization( self ):
-    self.setOptional( 'nomenclature' )
-    self.setOptional( 'mri_corrected' )
-    self.setOptional( 'white_mesh' )
-    self.setOptional( 'hemi_mesh' )
+def initialization(self):
+    self.setOptional('nomenclature')
+    self.setOptional('mri_corrected')
+    self.setOptional('white_mesh')
+    self.setOptional('hemi_mesh')
     self.load_MRI = "No"
     self.two_windows = "No"
-    self.linkParameters( 'white_mesh', 'graph' )
-    self.linkParameters( 'hemi_mesh', 'graph' )
-    self.linkParameters( 'mri_corrected', 'graph' )
-    self.nomenclature = self.signature[ 'nomenclature' ].findValue( {} )
+    self.linkParameters('white_mesh', 'graph')
+    self.linkParameters('hemi_mesh', 'graph')
+    self.linkParameters('mri_corrected', 'graph')
+    self.nomenclature = self.signature['nomenclature'].findValue({})
 
-def execution( self, context ):
+def execution(self, context):
     a = anatomist.Anatomist()
     selfdestroy = []
     if self.nomenclature is not None:
-        ( hie, br ) = context.runProcess( 'AnatomistShowNomenclature',
-                                          read=self.nomenclature )
-        selfdestroy += ( hie, br )
-    l = self.graph.get( 'automatically_labelled' )
-    context.write( 'automatically_labelled:', l )
+        (hie, br) = context.runProcess('AnatomistShowNomenclature',
+                                       read=self.nomenclature)
+        selfdestroy += (hie, br)
+    l = self.graph.get('automatically_labelled')
+    context.write('automatically_labelled:', l)
     nomenclatureprop = 'default'
     if l and l == 'Yes':
         nomenclatureprop = 'label'
-        #a.setGraphParams( label_attribute='label', use_nomenclature=1 )
+        #a.setGraphParams(label_attribute='label', use_nomenclature=1)
     else:
-        l = self.graph.get( 'manually_labelled' )
-        context.write( 'manually_labelled:', l )
+        l = self.graph.get('manually_labelled')
+        context.write('manually_labelled:', l)
         if l and l == 'Yes':
             nomenclatureprop = 'name'
-            #a.setGraphParams( label_attribute='name', use_nomenclature=1 )
-    graph = a.loadObject( self.graph )
-    context.write( 'nomenclature_property:', nomenclatureprop )
-    a.execute( 'GraphDisplayProperties', objects=[graph],
-      nomenclature_property=nomenclatureprop )
-    selfdestroy.append( graph )
+            #a.setGraphParams(label_attribute='name', use_nomenclature=1)
+    graph = a.loadObject(self.graph)
+    context.write('nomenclature_property:', nomenclatureprop)
+    a.execute('GraphDisplayProperties', objects=[graph],
+        nomenclature_property=nomenclatureprop)
+    selfdestroy.append(graph)
     mesh = None
     if self.load_MRI == "Yes":
         if self.mri_corrected is not None:
-            anat = a.loadObject( self.mri_corrected )
-            selfdestroy.append( anat )
+            anat = a.loadObject(self.mri_corrected)
+            selfdestroy.append(anat)
     if self.white_mesh is not None:
-        mesh = a.loadObject( self.white_mesh, duplicate=True )
-        selfdestroy.append( mesh )
-        mesh.setMaterial( a.Material(diffuse = [0.8, 0.8, 0.8, 1.]) )
+        mesh = a.loadObject(self.white_mesh, duplicate=True)
+        selfdestroy.append(mesh)
+        mesh.setMaterial(a.Material(diffuse=[0.8, 0.8, 0.8, 1.]))
     elif self.hemi_mesh is not None:
-        mesh = a.loadObject( self.hemi_mesh, duplicate=True )
-        selfdestroy.append( mesh )
-        mesh.setMaterial( a.Material(diffuse = [0.8, 0.8, 0.8, 0.5]) )
-    win3 = a.createWindow( '3D' )
+        mesh = a.loadObject(self.hemi_mesh, duplicate=True)
+        selfdestroy.append(mesh)
+        mesh.setMaterial(a.Material(diffuse=[0.8, 0.8, 0.8, 0.5]))
+    win3 = a.createWindow('3D')
     graphRef=graph.referential
-    win3.assignReferential( graphRef )
-    selfdestroy.append( win3 )
-    win3.addObjects( [graph], add_graph_nodes=True )
+    win3.assignReferential(graphRef)
+    selfdestroy.append(win3)
+    win3.addObjects([graph], add_graph_nodes=True)
     if mesh is not None:
-        win3.addObjects( [mesh] )
+        win3.addObjects([mesh])
         if self.two_windows == "Yes":
-            win2 = a.createWindow( '3D' )
-            win2.assignReferential( graphRef )
-            selfdestroy.append( win2 )
-            win2.addObjects( [graph] )
+            win2 = a.createWindow('3D')
+            win2.assignReferential(graphRef)
+            selfdestroy.append(win2)
+            win2.addObjects([graph])
         else :
             win2 = win3
     else:
         win2 = win3
     if self.load_MRI == "Yes":
         if self.mri_corrected is not None:
-            win2.addObjects( [anat] )
+            win2.addObjects([anat])
     #if self.nomenclature is not None:
         #wg= a.getDefaultWindowsGroup()
         ## to see the graph elements, we have to select them. After that they remain visible even if they are deselected
-        #wg.setSelectionByNomenclature( hie, ["unknown", "brain"] )
+        #wg.setSelectionByNomenclature(hie, ["unknown", "brain"])
         #wg.toggleSelectionByNomenclature(hie, ["unknown", "brain"])
     return selfdestroy
 
