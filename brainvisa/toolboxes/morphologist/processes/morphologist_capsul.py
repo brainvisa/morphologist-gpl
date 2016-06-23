a4a6fc30d14bce31ec5d2fd4b1d77cd121d0f97e
denisri <denis.riviere.gm@gmail.com>
2016-06-23 17:02:44 +0000

    adapt to future completion system of Capsul
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/trunk@73640 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/morphologist/processes/morphologist_capsul.py
+++ brainvisa/toolboxes/morphologist/processes/morphologist_capsul.py
@@ -7,6 +7,10 @@ name = 'Morphologist CAPSUL iteration'
 signature = Signature(
     't1mri', ListOf(ReadDiskItem('Raw T1 MRI',
                                   'aims readable volume formats')),
+    'capsul_process_type', OpenChoice(
+        ('Morphologist', 'morphologist.capsul.morphologist.Morphologist'),
+        ('Morphologist simple',
+         'morphologist.capsul.morphologist_simple.MorphologistSimple')),
     'analysis', ListOf(String()),
     'transfer_inputs', Boolean(),
     'transfer_outputs', Boolean(),
@@ -27,18 +31,19 @@ def onEditPipeline(self, process, dummy):
 
 def openPipeline(self):
     from capsul.qt_gui.widgets import PipelineDevelopperView
-    from morphologist.capsul.morphologist import Morphologist
     from capsul.api import Pipeline
     Pipeline.hide_nodes_activation = False
-    if self._edited_pipeline is None:
-        self._edited_pipeline = Morphologist()
-        self._edited_pipeline.nodes_activation.SulciRecognition = True
-        self._edited_pipeline.nodes_activation.SulciRecognition_1 = True
     mpv = PipelineDevelopperView(
-      self._edited_pipeline, allow_open_controller=True,
+      self.get_edited_pipeline(), allow_open_controller=True,
       show_sub_pipelines=True)
     mpv.show()
     self._pipeline_view = MainThreadLife(mpv)
+
+
+def change_process_type(self, process, dummy):
+    if self.edit_one_pipeline:
+        self.edit_one_pipeline = False
+    self._edited_pipeline = None
 
 
 def initialization(self):
@@ -50,6 +55,110 @@ def initialization(self):
     self._edited_pipeline = None
     self._pipeline_view = None
     self.linkParameters(None, 'edit_one_pipeline', self.onEditPipeline)
+    self.linkParameters(None, 'capsul_process_type', self.change_process_type)
+
+
+def customize_process(self):
+    if self.capsul_process_type \
+            == 'morphologist.capsul.morphologist.Morphologist':
+        self._edited_pipeline.nodes_activation.SulciRecognition = True
+        self._edited_pipeline.nodes_activation.SulciRecognition_1 = True
+    elif self.capsul_process_type \
+            == 'morphologist.capsul.morphologist_simple.MorphologistSimple':
+        self._edited_pipeline.method_ACPC = 'With SPM Normalization'
+
+
+def get_initial_study_config(self):
+    from soma.wip.application.api import Application as Appli2
+    configuration = Appli2().configuration
+    init_study_config = {
+        "input_directory" : '/tmp',
+        "output_directory" : '/tmp',
+        "input_fom" : "morphologist-auto-1.0",
+        "output_fom" : "morphologist-auto-1.0",
+        "shared_fom" : "shared-brainvisa-1.0",
+        "spm_directory" : configuration.SPM.spm8_standalone_path,
+        "use_soma_workflow" : True,
+        "use_fom" : True,
+        "volumes_format" : 'NIFTI gz',
+        "meshes_format" : "GIFTI",
+    }
+    return init_study_config
+
+
+def get_edited_pipeline(self):
+    if self._edited_pipeline is None:
+        from capsul import info as cinfo
+        cversion = (cinfo.version_major, cinfo.version_minor,
+                    cinfo.version_micro)
+        if cversion[0] >= 2:
+            from capsul.api import get_process_instance
+        else:
+            from capsul.process import get_process_instance
+        if cversion >= (2, 1):
+            from capsul.attributes.completion_engine \
+                import ProcessCompletionEngine
+        self._edited_pipeline = get_process_instance(self.capsul_process_type)
+        self.customize_process()
+        if cversion >= (2, 1):
+            from capsul.study_config.study_config import StudyConfig
+            init_study_config = self.get_initial_study_config()
+            study_config = StudyConfig(
+                init_config=init_study_config,
+                modules=StudyConfig.default_modules
+                    + ['BrainVISAConfig', 'FomConfig'])
+            self._edited_pipeline.set_study_config(study_config)
+            pf = ProcessCompletionEngine.get_completion_engine(
+                self._edited_pipeline)
+            self._edited_pipeline.completion_engine = pf
+
+    return self._edited_pipeline
+
+
+def get_initial_study_config(self):
+    from soma.wip.application.api import Application as Appli2
+    configuration = Appli2().configuration
+    init_study_config = {
+        "input_directory" : '/tmp',
+        "output_directory" : '/tmp',
+        "input_fom" : "morphologist-auto-1.0",
+        "output_fom" : "morphologist-auto-1.0",
+        "shared_fom" : "shared-brainvisa-1.0",
+        "spm_directory" : configuration.SPM.spm8_standalone_path,
+        "use_soma_workflow" : True,
+        "use_fom" : True,
+        "volumes_format" : 'NIFTI gz',
+        "meshes_format" : "GIFTI",
+    }
+    return init_study_config
+
+
+def get_edited_pipeline(self):
+    if self._edited_pipeline is None:
+        from capsul import info as cinfo
+        cversion = (cinfo.version_major, cinfo.version_minor,
+                    cinfo.version_micro)
+        from capsul.api import get_process_instance
+        if cversion >= (2, 1):
+            from capsul.attributes.completion_engine \
+                import ProcessCompletionEngine
+        self._edited_pipeline = get_process_instance(
+            'morphologist.capsul.morphologist.Morphologist')
+        self._edited_pipeline.nodes_activation.SulciRecognition = True
+        self._edited_pipeline.nodes_activation.SulciRecognition_1 = True
+        if cversion >= (2, 1):
+            from capsul.study_config.study_config import StudyConfig
+            init_study_config = self.get_initial_study_config()
+            study_config = StudyConfig(
+                init_config=init_study_config,
+                modules=StudyConfig.default_modules
+                    + ['BrainVISAConfig', 'FomConfig'])
+            self._edited_pipeline.set_study_config(study_config)
+            pf = ProcessCompletionEngine.get_completion_engine(
+                self._edited_pipeline)
+            self._edited_pipeline.completion_engine = pf
+
+    return self._edited_pipeline
 
 
 def execution(self, context):
@@ -57,8 +166,12 @@ def execution(self, context):
     import time
     from soma.application import Application
     from capsul.study_config.study_config import StudyConfig
-    from morphologist.capsul.morphologist import Morphologist
-    from capsul.process import process_with_fom
+    from capsul import info as cinfo
+    cversion = (cinfo.version_major, cinfo.version_minor, cinfo.version_micro)
+    if cversion >= (2, 1):
+        from capsul.attributes.completion_engine import ProcessCompletionEngine
+    else:
+        from capsul.process import process_with_fom
     from soma_workflow import client as swclient
     from soma.wip.application.api import Application as Appli2
     import numpy as np
@@ -68,17 +181,25 @@ def execution(self, context):
     #soma_app.initialize()
     configuration = Appli2().configuration
 
-    axon_to_capsul_formats = {
-        'NIFTI-1 image': "NIFTI",
-        'gz compressed NIFTI-1 image': "NIFTI gz",
-        'GIS image': "GIS",
-        'MINC image': "MINC",
-        'SPM image': "SPM",
-        'GIFTI file': "GIFTI",
-        'MESH mesh': "MESH",
-        'PLY mesh': "PLY",
-        'siRelax Fold Energy': "Energy",
-    }
+    if self.capsul_process_type \
+            == 'morphologist.capsul.morphologist.Morphologist':
+        # Morphologist uses the hand-written Capsul formats
+        # formats-brainvisa-1.0
+        axon_to_capsul_formats = {
+            'NIFTI-1 image': "NIFTI",
+            'gz compressed NIFTI-1 image': "NIFTI gz",
+            'GIS image': "GIS",
+            'MINC image': "MINC",
+            'SPM image': "SPM",
+            'GIFTI file': "GIFTI",
+            'MESH mesh': "MESH",
+            'PLY mesh': "PLY",
+            'siRelax Fold Energy': "Energy",
+        }
+    else:
+        # other processes use the directly translated formats
+        # brainvisa-formats-3.2.0
+        axon_to_capsul_formats = {}
 
     # dirs and formats have to be handled sorted since
     # FomConfig.check_and_update_foms(study_config) needs to be called
@@ -109,19 +230,21 @@ def execution(self, context):
         "meshes_format" : "GIFTI",
     }
 
-    study_config = StudyConfig(
-        init_config=init_study_config,
-        modules=StudyConfig.default_modules + ['BrainVISAConfig', 'FomConfig'])
-
-    if self._edited_pipeline is not None:
-        mp = self._edited_pipeline
+    mp = self.get_edited_pipeline()
+    if cversion >= (2, 1):
+        study_config = mp.get_study_config()
+        study_config.set_study_configuration(init_study_config)
+        pf = mp.completion_engine
     else:
-        mp = Morphologist()
-        mp.nodes_activation.SulciRecognition = True
-        mp.nodes_activation.SulciRecognition_1 = True
-    pf = process_with_fom.ProcessWithFom(mp, study_config)
+        study_config = StudyConfig(
+            init_config=init_study_config,
+            modules=StudyConfig.default_modules
+                + ['BrainVISAConfig', 'FomConfig'])
+        pf = process_with_fom.ProcessWithFom(mp, study_config)
+
     # activate normalization methods disabling
-    mp.attach_config_activations(study_config)
+    if hasattr(mp, 'attach_config_activations'):
+        mp.attach_config_activations(study_config)
 
     # workflow config
     from capsul.pipeline import pipeline_workflow
@@ -136,7 +259,7 @@ def execution(self, context):
     study_config.somaworkflow_computing_resources_config.localhost \
         = path_translations
 
-    workflow = swclient.Workflow(name='Morphologist CAPSUL iteration', jobs=[])
+    workflow = swclient.Workflow(name='%s CAPSUL iteration' % mp.name, jobs=[])
     workflow.root_group = []
 
     context.progress(0, len(self.t1mri), process=self)
@@ -145,13 +268,17 @@ def execution(self, context):
         t1mri = self.t1mri[i]
         format = formats[i]
         database = t1mri['_database']
-        pf.attributes['center'] = t1mri['center']
-        pf.attributes['subject'] = t1mri['subject']
-        pf.attributes['acquisition'] = t1mri['acquisition']
+        if cversion >= (2, 1):
+            attributes = {}
+        else:
+            attributes = pf.attributes
+        attributes['center'] = t1mri['center']
+        attributes['subject'] = t1mri['subject']
+        attributes['acquisition'] = t1mri['acquisition']
         j = i
         if len(self.analysis) <= j:
             j = -1
-        pf.attributes['analysis'] = self.analysis[j]
+        attributes['analysis'] = self.analysis[j]
         # handle input format
         if database != old_database or format != old_format:
             # need to reconfigure FOMs for this new dirs/format
@@ -163,7 +290,10 @@ def execution(self, context):
             study_config.initialize_modules()
         format = axon_to_capsul_formats.get(t1mri.format.name,
                                             t1mri.format.name)
-        pf.create_completion()
+        if cversion >= (2, 1):
+            pf.complete_parameters(mp, {'capsul_attributes': attributes})
+        else:
+            pf.create_completion()
 
         transfers = []
         if self.transfer_inputs \
@@ -182,11 +312,11 @@ def execution(self, context):
 
         priority = (len(self.t1mri) - item - 1) * 100
         wf = pipeline_workflow.workflow_from_pipeline(
-            pf.process, study_config=study_config)  #, jobs_priority=priority)
+            mp, study_config=study_config)  #, jobs_priority=priority)
         workflow.jobs += wf.jobs
         workflow.dependencies += wf.dependencies
         group = swclient.Group(wf.root_group,
-                               name='Morphologist iter %i' % i)
+                               name='%s iter %i' % (mp.name, i))
         workflow.root_group.append(group) # += wf.root_group
         workflow.groups += [group] + wf.groups
         context.progress(item+1, len(self.t1mri), process=self)
