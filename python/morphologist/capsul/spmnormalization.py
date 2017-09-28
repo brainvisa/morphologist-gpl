62c69cb3397585d2ab93e9ed60b40a323569f375
denisri <denis.riviere.gm@gmail.com>
2017-09-28 00:33:57 +0000

    * use a converter in SPM normalization pipeline to convert to uncompressed nifti * fixed morphologist_viewer after another fix in anatomist
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@79241 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/morphologist/capsul/spmnormalization.py
+++ python/morphologist/capsul/spmnormalization.py
@@ -18,8 +18,42 @@ class SPMNormalization(SPMnormalizationPipeline):
             '->ConvertSPMnormalizationToAIMS.read')
         self.add_link(
             'ConvertSPMnormalizationToAIMS.write->transformation')
+
+        # add converter as 1st step
+        self.remove_link('t1mri->normalization_t1_spm12_reinit.anatomy_data')
+        self.remove_link('t1mri->normalization_t1_spm8_reinit.anatomy_data')
+        self.add_process(
+            'converter',
+            'morphologist.capsul.axon.aimsconverter.AimsConverter')
+        self.add_link('t1mri->converter.read')
+        self.add_link('converter.write->'
+                      'normalization_t1_spm12_reinit.anatomy_data')
+        self.add_link('converter.write->'
+                      'normalization_t1_spm8_reinit.anatomy_data')
+
         self.nodes_activation.ReorientAnatomy = True
         self.on_trait_change(self.change_flip, 'allow_flip_initial_MRI')
+        self.nodes['converter'].process.trait('write').allowed_extensions \
+            = ['.nii']
+        self.nodes['converter'].process.trait('write').optional = False
+        self.nodes['converter'].process.trait('removeSource').optional = True
+        self.nodes['converter'].process.trait('ascii').optional = True
+        self.nodes['converter'].process.trait('rescaleDynamic').optional = True
+        self.nodes['converter'].process.trait('useInputTypeLimits').optional = True
+        self.nodes['converter'].plugs['removeSource'].optional = True
+        self.nodes['converter'].plugs['ascii'].optional = True
+        self.nodes['converter'].plugs['rescaleDynamic'].optional = True
+        self.nodes['converter'].plugs['useInputTypeLimits'].optional = True
+
+        self.node_position = {
+            'ConvertSPMnormalizationToAIMS': (1302., 99.),
+            'NormalizeSPM': (863., 195.),
+            'ReorientAnatomy': (1587., 9.),
+            'converter': (417., 206.),
+            'inputs': (0.0, 0.0),
+            'normalization_t1_spm12_reinit': (598., 172.),
+            'normalization_t1_spm8_reinit': (599., 424.),
+            'outputs': (1876., 153.)}
 
     def change_flip(self, value):
         self.nodes_activation.ReorientAnatomy = value
