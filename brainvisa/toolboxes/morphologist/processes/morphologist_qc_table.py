80869d88d04d2eb72ea85f85efc8c0f7fc1620e2
denisri <denis.riviere.gm@gmail.com>
2017-10-14 01:57:14 +0000

    specialization of database_qc_table, dedicated to morphologist results
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@79460 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/morphologist/processes/morphologist_qc_table.py
@@ -0,0 +1,77 @@
+ 
+from brainvisa.processes import *
+
+name = 'Morphologist QC table'
+userLevel = 0
+
+signature = Signature(
+    'database', Choice(),
+    'keys', ListOf(String()),
+    'acquisition', String(),
+    'analysis', String(),
+    'graph_version', String(),
+    'data_filters', ListOf(String()),
+)
+
+
+def initialization(self):
+    # list of possible databases, while respecting the ontology
+    # ontology: brainvisa-3.2.0
+    databases = [h.name for h in neuroHierarchy.hierarchies()]
+    self.signature["database"].setChoices(*databases)
+    if len(databases) >= 2:
+        self.database = databases[1]
+    else:
+        self.signature["database"] = OpenChoice()
+
+    self.setOptional('data_filters', 'acquisition', 'analysis',
+                     'graph_version')
+    self.keys = ['subject']
+
+
+def execution(self, context):
+    dtypes = ['Raw T1 MRI', 'T1 MRI Bias Corrected', 'Histo Analysis',
+              'T1 Brain Mask', 'Split Brain Mask', 'Head Mesh',
+              'Left Grey White Mask', 'Right Grey White Mask',
+              'Left CSF+GREY Mask', 'Right CSF+GREY Mask',
+              'Left Hemisphere White Mesh', 'Right Hemisphere White Mesh',
+              'Left Cortex Skeleton', 'Right Cortex Skeleton',
+              'Left Hemisphere Mesh', 'Right Hemisphere Mesh',
+              'Left Cortical folds graph', 'Right Cortical folds graph',
+              'Labelled Cortical folds graph', 'Labelled Cortical folds graph',
+              'Sulcal morphometry measurements']
+
+    filter1 = {}
+    if self.acquisition:
+        filter1.update({'acquisition': self.acquisition})
+    filter2 = dict(filter1)
+    filter1['normalized'] = 'no'
+    if self.analysis:
+        filter2.update({'analysis': self.analysis})
+    filter3_l = dict(filter2)
+    filter3_l.update({'side': 'left'})
+    filter3_r = dict(filter2)
+    filter3_r.update({'side': 'right'})
+    filter4_l = dict(filter3_l)
+    if self.graph_version:
+        filter4_l.update({'graph_version': self.graph_version})
+    filter4_r = dict(filter3_r)
+    if self.graph_version:
+        filter4_r.update({'graph_version': self.graph_version})
+    filter5 = dict(filter2)
+    if self.graph_version:
+        filter2.update({'graph_version': self.graph_version})
+
+    filters = [filter1, filter2, filter2, filter2, filter2, filter2,
+               filter3_l, filter3_r, filter3_l, filter3_r, filter3_l,
+               filter3_r, filter3_l, filter3_r, filter3_l, filter3_r,
+               filter4_l, filter4_r, filter4_l, filter4_r, filter5]
+    filters = [repr(filt) for filt in filters]
+
+    context.write('filters:', filters)
+    self.proc = getProcessInstance('database_qc_table')
+    return context.runProcess(self.proc, database=self.database,
+                              data_types=dtypes,
+                              data_filters=filters,
+                              keys=self.keys)
+
