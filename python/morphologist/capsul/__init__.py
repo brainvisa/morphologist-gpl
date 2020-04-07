7b0a710f26bfdc8e083c7a517d90a6f855a0cb87
denisri <denis.riviere.gm@gmail.com>
2020-04-07 14:57:22 +0000

    set _doc_path in capsul processes module
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/morphologist-gpl/branches/bug_fix@86332 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/morphologist/capsul/__init__.py
@@ -0,0 +1,29 @@
+
+_doc_path = None
+
+def _init_doc_path():
+    global _doc_path
+    import os
+    import morphologist
+
+    # use axon config for version
+    try:
+        import brainvisa.config as bv_config
+        axon_version = bv_config.shortVersion
+    except:
+        axon_version = '4.6'
+
+    opd = os.path.dirname
+    p = os.path.join(
+        opd(opd(opd(morphologist.__file__))),
+        'share/doc/morphologist-%s/dev_doc/process_docs/morphologist/capsul' \
+            % axon_version)
+    if os.path.exists(p):
+        _doc_path = p
+        return _doc_path
+    _doc_path = 'http://brainvisa.info/morphologist-%s/dev_doc/process_docs/' \
+        'morphologist/capsul' % axon_version
+    return _doc_path
+
+_init_doc_path()
+
