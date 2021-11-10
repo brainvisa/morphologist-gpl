b5edf4ea8182759e84c80c82b2b0017542c12b3a
Denis Rivière <denis.riviere.gm@gmail.com>
2021-11-10 17:04:53 +0100

    removed useless imports

--- brainvisa/toolboxes/morphologist/processes/morphologist-ui_database.py
+++ brainvisa/toolboxes/morphologist/processes/morphologist-ui_database.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from brainvisa.processes import *
-from brainvisa.processing.qtgui.backwardCompatibleQt import *
 
 
 def validation():
