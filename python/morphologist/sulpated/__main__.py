a0f6bde19b3fc988f795a594a5e4652f17b7d9e0
Denis Rivière <denis.riviere.gm@gmail.com>
2023-03-23 18:01:21 +0100

    fix in doc (-o param is not a BV database but the patterns dir)

--- python/morphologist/sulpated/__main__.py
+++ python/morphologist/sulpated/__main__.py
@@ -98,8 +98,7 @@ parser = ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
     'user has to do it by his own. Normally it should not happen often.'
 )
 parser.add_argument('-o', '--output',
-                    help='output brainvisa database location '
-                    '[default: current directory]')
+                    help='output data location [default: current directory]')
 parser.add_argument('-s', '--sulci_db',
                     help='input/output sulci brainvisa database location')
 parser.add_argument('-i', '--input_db',
