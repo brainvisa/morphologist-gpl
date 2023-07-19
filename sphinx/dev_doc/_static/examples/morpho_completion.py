845d86998c1a943d34e32a785c05a5d6a514785e
denisri <denis.riviere.gm@gmail.com>
2023-07-19 19:27:40 +0200

    rewriting the example script for morphologist completion

--- sphinx/dev_doc/_static/examples/morpho_completion.py
+++ sphinx/dev_doc/_static/examples/morpho_completion.py
@@ -1,42 +1,90 @@
 #!/usr/bin/env python
 
-from capsul.api import capsul_engine
-from capsul.attributes.completion_engine import ProcessCompletionEngine
+from capsul.api import Capsul
+import json
 from soma.qt_gui.qt_backend import Qt
-from soma.qt_gui.controller_widget import ScrollControllerWidget
-import pickle
+from capsul.dataset import ProcessMetadata, BrainVISASchema
+from capsul.schemas.brainvisa import (declare_morpho_schemas,
+                                      morphologist_datasets)
+
 
 show_links_debugger = False
-show_fom_gui = True
+show_compl_gui = True
 show_pipeline = False
 
-engine = capsul_engine()
-engine.load_modules(['fom', 'axon'])
-with engine.settings as session:
-    config = session.config('fom', 'global')
-    config.input_fom = 'morphologist-auto-nonoverlap-1.0'
-    config.output_fom = 'morphologist-auto-nonoverlap-1.0'
-    config.input_directory = '/tmp/input_data'
-    config.output_directory = '/tmp/output_data'
+real_morpho_module = 'morphologist.capsul'
+fake_morpho_module = 'capsul.pipeline.test.fake_morphologist'
+#morpho_module = fake_morpho_module
+morpho_module = real_morpho_module
 
 
-mp = engine.get_process_instance(
-  'morphologist.capsul.morphologist.Morphologist')
-# set this if you want to use custom individual files as input(s): this
-# will disable completion for the input T1 parameter (before importation):
-# mp.trait('t1mri').forbid_completion = True
+def get_shared_path():
+    try:
+        from soma import aims
+        return aims.carto.Paths.resourceSearchPath()[-1]
+    except Exception:
+        return '!{dataset.shared.path}'
 
+
+declare_morpho_schemas(morpho_module)
+
+capsul = Capsul()
+engine = capsul.engine()
+config = capsul.config
+
+config.builtin.add_module('spm')
+config.builtin.add_module('axon')
+config.import_dict({
+    'builtin': {
+        'config_modules': [
+            'spm',
+            'axon',
+        ],
+        'dataset': {
+            'input': {
+                'path': '/tmp/morpho-bids',
+                'metadata_schema': 'bids',
+            },
+            'output': {
+                'path': '/tmp/morpho-bv',
+                'metadata_schema': 'brainvisa',
+            },
+            'shared': {
+                'path': get_shared_path(),
+                'metadata_schema': 'brainvisa_shared',
+            },
+        },
+        'spm': {
+            'spm12_standalone': {
+                'directory': '/tmp/spm12-standalone',
+                'standalone': True,
+                'version': '12',
+            }
+        },
+        'matlab': {
+            'matlab_mcr': {
+                'mcr_directory': '/tmp/matlab_mcr/v97',
+            }
+        },
+    }
+})
+
+
+mp = capsul.executable('%s.morphologist.Morphologist' % morpho_module)
+
+execution_context = engine.execution_context(mp)
+# get metadata from an input t1mri path in the input BIDS database
+input = '/tmp/morpho-bids/rawdata/sub-aleksander/ses-m0/anat/' \
+    'sub-aleksander_ses-m0_T1w.nii.gz'
 # completion API
-pc = ProcessCompletionEngine.get_completion_engine(mp)
-attributes = pc.get_attribute_values()
-attributes.center = 'subjects'
-attributes.subject = 'irm2'
-pc.complete_parameters()
+input_metadata = execution_context.dataset['input'].schema.metadata(input)
 
-if not show_fom_gui:
-    # if show_fom_gui is on, the AttributedProcessWidget will connect this
-    # (which is questionable)
-    pc.capsul_attributes.on_trait_change(pc.attributes_changed)
+metadata = ProcessMetadata(mp, execution_context,
+                           datasets=morphologist_datasets)
+# set input metadata in the pipeline metadata set
+metadata.bids = input_metadata
+# run completion
+metadata.generate_paths(mp)
 
 if Qt.QApplication.instance() is not None:
 
@@ -52,7 +100,7 @@ if Qt.QApplication.instance() is not None:
         from capsul.qt_gui.widgets import links_debugger
 
         ldv = links_debugger.CapsulLinkDebuggerView(mp)
-        #mp.t1mri = '/tmp/totor.nii'
+        #mp.t1mri = '/tmp/totor.nii.gz'
         #ldv.update_links_view()
         ldv.show()
 
@@ -64,29 +112,42 @@ if Qt.QApplication.instance() is not None:
         pcv.show()
 
 # iteration
+non_iterative_plugs = [f.name for f in mp.fields()
+                       if f.name in morphologist_datasets
+                       and morphologist_datasets.get(f.name)
+                            in ('shared', None)]
+pipeline = capsul.executable_iteration(
+    '%s.morphologist.Morphologist' % morpho_module,
+    non_iterative_plugs=non_iterative_plugs
+)
+execution_context = engine.execution_context(pipeline)
 
-pipeline = engine.get_iteration_pipeline('iter', 'morpho', mp, ['t1mri'])
-cm = ProcessCompletionEngine.get_completion_engine(pipeline)
-cm.get_attribute_values().subject = ['s1', 's2', 's3']
-cm.complete_parameters()
+paths = [
+    '/tmp/morpho-bids/rawdata/sub-aleksander/ses-m0/anat/'
+    'sub-aleksander_ses-m0_T1w.nii.gz',
+    '/tmp/morpho-bids/rawdata/sub-aleksander/ses-m10/anat/'
+    'sub-aleksander_ses-m10_T1w.nii.gz',
+    '/tmp/morpho-bids/rawdata/sub-casimir/ses-m10/anat/'
+    'sub-casimir_ses-m10_T1w.nii.gz',
+    '/tmp/morpho-bids/rawdata/sub-calimero/ses-m10/anat/'
+    'sub-calimero_ses-m10_T1w.nii.gz',
+    '/tmp/morpho-bids/rawdata/sub-calimero/ses-m18/anat/'
+    'sub-calimero_ses-m18_T1w.nii.gz',
+]
+iter_meta_bids = []
+
+for path in paths:
+    input_metadata = execution_context.dataset['input'].schema.metadata(path)
+    iter_meta_bids.append(input_metadata)
+
+metadata = ProcessMetadata(pipeline, execution_context,
+                           datasets=morphologist_datasets, debug=False)
+metadata.bids = iter_meta_bids
+metadata.generate_paths(pipeline)
+
 if show_fom_gui and Qt.QApplication.instance() is not None:
     pcvi = AttributedProcessWidget(pipeline, enable_attr_from_filename=True,
                                   enable_load_buttons=True)
     pcvi.show()
 
-
-#workflow
-#from capsul.pipeline import pipeline_workflow
-#study_config.somaworkflow_computing_resource = 'localhost'
-#study_config.somaworkflow_computing_resources_config = {
-    #'localhost': {
-        #'transfer_paths': [study_config.input_directory],
-        #'path_translations' : {
-            #study_config.shared_directory:
-                #['brainvisa', 'de25977f-abf5-9f1c-4384-2585338cd7af']}
-    #}
-#}
-#wf = pipeline_workflow.workflow_from_pipeline(pipeline)
-#with open('/tmp/morphoiter.workflow', 'wb') as f:
-    #pickle.dump(wf, f)
 
